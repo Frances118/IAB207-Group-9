d60809d590a20f60bcab526756ef941b5fd31daa
Olivia Zheng <99567408+Oli1021@users.noreply.github.com>
2023-06-19 19:08:11 +1000

    Update index.py

--- App/controllers/index/index.py
+++ App/controllers/index/index.py
@@ -1,6 +1,8 @@
 import datetime
 
-from flask import render_template, Blueprint
+from flask import redirect, render_template, Blueprint, url_for
+from sqlalchemy import func
+from App.models.forms import SearchForm
 
 from App.controllers.common.utils import to_json
 from App.models.models import Concert
@@ -8,15 +10,13 @@ from App.models.models import Concert
 index_index = Blueprint('index_index', __name__, )
 
 
-@index_index.route('', methods=['GET', 'POST'])
-@index_index.route('index/', methods=['GET', 'POST'])
-@index_index.route('index/index/', methods=['GET', 'POST'])
 @index_index.route('index/index.html', methods=['GET', 'POST'])
 def index():
+    search_form = SearchForm()
     today = datetime.datetime.now().strftime('%Y-%m-%d')
-    events = Concert.query.filter(Concert.eventDate == today).order_by(Concert.id.desc()).all()
 
-    Upcoming = Concert.query.filter(Concert.eventDate > today).order_by(Concert.id.desc()).all()
+    events = Concert.query.filter(func.strftime('%Y-%m-%d', Concert.eventDate) == today).order_by(Concert.id.desc()).all()
+    Upcoming = Concert.query.filter(func.strftime('%Y-%m-%d', Concert.eventDate) > today).order_by(Concert.id.desc()).all()
 
     rows = Concert.query.filter().order_by(Concert.id.desc()).all()
     categorys = []
@@ -30,4 +30,15 @@ def index():
             categorys.append(item['category'])
             datas.append(item)
 
-    return render_template('index/index.html', **{'events': events, 'Upcoming': Upcoming, 'datas': datas})
+    return render_template('index/index.html', search_form=search_form, events= events, Upcoming= Upcoming, datas=datas)
+
+@index_index.route('/event_detail', methods=['GET', 'POST'])
+def search_event():
+    search_form = SearchForm()
+    if search_form.validate_on_submit():
+        keyword = search_form.keyword.data
+        concert_detail = Concert.query.filter(Concert.eventName.like('%'+keyword+'%')).first()
+        if concert_detail:
+            return redirect(url_for('index_event.detail', id=concert_detail.id))
+
+    return redirect(url_for('index_index.index'))
