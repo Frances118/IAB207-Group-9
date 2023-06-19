2b38e919cbe5d647749ad6f31ac1bb9c23a0f374
Frances118 <133765404+Frances118@users.noreply.github.com>
2023-06-19 18:24:36 +1000

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -6,6 +6,8 @@ from App.models.models import db, User
 from flask_login import logout_user, current_user, UserMixin, LoginManager, login_manager, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
 
 def register_plugin(app):
     db.init_app(app)
@@ -14,7 +16,7 @@ def register_plugin(app):
 
 
 def create_app():
-    app = Flask(__name__, static_folder='static', static_url_path='/static')
+
     app.config.from_object('config.dbs')
     app.secret_key = '111111'
     login_manager = LoginManager()
@@ -22,21 +24,23 @@ def create_app():
     login_manager.login_view = 'index_login.index'
     login_manager.init_app(app)
 
+    @app.login_manager.user_loader
+    def user_loader(user_id):
+        return User.query.get(user_id)
+
     register_blueprints(app)
     register_plugin(app)
+    index_middleware(app)
+
     return app
 
 
-app = create_app()
-index_middleware(app)
-
-
-@app.login_manager.user_loader
-def user_loader(user_id):
-    return User.query.get(user_id)
+# app = create_app()
+# index_middleware(app)
 
 
 @app.route('/')
 def home():
     return redirect("/index/index/index.html")
 #    return render_template("base.html")
+
